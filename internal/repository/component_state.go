5a66903077fa18f360365b6b1bc9143c41128f1e
karminski <code.karminski@outlook.com>
2022-07-14 16:11:26 +0800

    ADD: testing case for BuildComponentTree().

---
+++ internal/repository/component_state.go
@@ -0,0 +1,97 @@
+// Copyright 2022 The ILLA Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package repository
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+)
+
+type ComponentNode struct {
+	DisplayName    string                 `json:"displayName"`
+	ParentNode     string                 `json:"parentNode"`
+	ShowName       string                 `json:"showName"`
+	Cerror         bool                   `json:"error"`
+	IsDragging     bool                   `json:"isDragging"`
+	ChildrenNode   []*ComponentNode       `json:"childrenNode"`
+	Ctype          string                 `json:"type"`
+	ContainerType  map[string]interface{} `json:"containerType"`
+	VerticalResize bool                   `json:"verticalResize"`
+	H              int                    `json:"h"`
+	W              int                    `json:"w"`
+	MinH           int                    `json:"minH"`
+	MinW           int                    `json:"minW"`
+	X              int                    `json:"x"`
+	Y              int                    `json:"y"`
+	Z              int                    `json:"z"`
+	Props          map[string]interface{} `json:"props"`
+	PanelConfig    map[string]interface{} `json:"panelConfig"`
+}
+
+func NewComponentNodeFromJSON(cnodebyte []byte) (*ComponentNode, error) {
+	cnode := ComponentNode{}
+	if err := json.Unmarshal(cnodebyte, &cnode); err != nil {
+		return nil, err
+	}
+	return &cnode, nil
+}
+
+func (cnode *ComponentNode) UpdateParentNode(parentComponentNode *ComponentNode) {
+	if parentComponentNode != nil {
+		cnode.ParentNode = parentComponentNode.DisplayName
+	}
+}
+
+func (cnode *ComponentNode) AppendChildrenNode(node *ComponentNode) {
+	cnode.ChildrenNode = append(cnode.ChildrenNode, node)
+}
+
+func (cnode *ComponentNode) Serialization() ([]byte, error) {
+	// build all relations for client output serialization
+	return json.Marshal(cnode)
+}
+
+func (cnode *ComponentNode) SerializationForDatabase() ([]byte, error) {
+	// the parentNode and childrenNode relation info are storaged in special column in database.
+	// build these relations for client output serialization only.
+	cnode.ParentNode = ""
+	cnode.ChildrenNode = nil
+	return json.Marshal(cnode)
+}
+
+func BuildComponentTree(treeState *TreeState, treeStateMap map[int]*TreeState, parentComponentNode *ComponentNode) (*ComponentNode, error) {
+	cnode := &ComponentNode{}
+	var err error
+	if cnode, err = treeState.ExportContentAsComponentState(); err != nil {
+		return nil, err
+	}
+	for _, id := range treeState.ChildrenNodeRefIDs {
+		subcnode := &ComponentNode{}
+		var err error
+		// check if children nodes is exists
+		subTreeState, ok := treeStateMap[id]
+		if !ok {
+			return nil, errors.New("TreeState relation has broken, can not find children node id: " + strconv.Itoa(id))
+		}
+		if subcnode, err = BuildComponentTree(subTreeState, treeStateMap, cnode); err != nil {
+			return nil, err
+		}
+		cnode.AppendChildrenNode(subcnode)
+	}
+	cnode.UpdateParentNode(parentComponentNode)
+	return cnode, nil
+
+}
---
+++ internal/repository/component_state_test.go
@@ -0,0 +1,95 @@
+// Copyright 2022 The ILLA Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package repository
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildComponentTree(t *testing.T) {
+	serilizationResult := `{"displayName":"cnode1","parentNode":"","showName":"","error":false,"isDragging":false,"childrenNode":[{"displayName":"cnode2","parentNode":"cnode1","showName":"","error":false,"isDragging":false,"childrenNode":[{"displayName":"cnode4","parentNode":"cnode2","showName":"","error":false,"isDragging":false,"childrenNode":[{"displayName":"cnode5","parentNode":"cnode4","showName":"","error":false,"isDragging":false,"childrenNode":null,"type":"","containerType":null,"verticalResize":false,"h":0,"w":0,"minH":0,"minW":0,"x":0,"y":0,"z":0,"props":null,"panelConfig":null}],"type":"","containerType":null,"verticalResize":false,"h":0,"w":0,"minH":0,"minW":0,"x":0,"y":0,"z":0,"props":null,"panelConfig":null}],"type":"","containerType":null,"verticalResize":false,"h":0,"w":0,"minH":0,"minW":0,"x":0,"y":0,"z":0,"props":null,"panelConfig":null},{"displayName":"cnode3","parentNode":"cnode1","showName":"","error":false,"isDragging":false,"childrenNode":null,"type":"","containerType":null,"verticalResize":false,"h":0,"w":0,"minH":0,"minW":0,"x":0,"y":0,"z":0,"props":null,"panelConfig":null}],"type":"","containerType":null,"verticalResize":false,"h":0,"w":0,"minH":0,"minW":0,"x":0,"y":0,"z":0,"props":null,"panelConfig":null}`
+	// init component node
+	cnode1 := ComponentNode{
+		DisplayName: "cnode1",
+	}
+	cnode1str, _ := json.Marshal(cnode1)
+	cnode2 := ComponentNode{
+		DisplayName: "cnode2",
+	}
+	cnode2str, _ := json.Marshal(cnode2)
+	cnode3 := ComponentNode{
+		DisplayName: "cnode3",
+	}
+	cnode3str, _ := json.Marshal(cnode3)
+	cnode4 := ComponentNode{
+		DisplayName: "cnode4",
+	}
+	cnode4str, _ := json.Marshal(cnode4)
+	cnode5 := ComponentNode{
+		DisplayName: "cnode5",
+	}
+	cnode5str, _ := json.Marshal(cnode5)
+
+	// init tree state data
+	treeState1 := TreeState{
+		ID:                 1,
+		StateType:          TREE_STATE_TYPE_COMPONENTS,
+		ParentNodeRefID:    0,
+		ChildrenNodeRefIDs: []int{2, 3},
+		Content:            string(cnode1str),
+	}
+	treeState2 := TreeState{
+		ID:                 2,
+		StateType:          TREE_STATE_TYPE_COMPONENTS,
+		ParentNodeRefID:    0,
+		ChildrenNodeRefIDs: []int{4},
+		Content:            string(cnode2str),
+	}
+	treeState3 := TreeState{
+		ID:                 3,
+		StateType:          TREE_STATE_TYPE_COMPONENTS,
+		ParentNodeRefID:    0,
+		ChildrenNodeRefIDs: []int{},
+		Content:            string(cnode3str),
+	}
+	treeState4 := TreeState{
+		ID:                 4,
+		StateType:          TREE_STATE_TYPE_COMPONENTS,
+		ParentNodeRefID:    0,
+		ChildrenNodeRefIDs: []int{5},
+		Content:            string(cnode4str),
+	}
+	treeState5 := TreeState{
+		ID:                 5,
+		StateType:          TREE_STATE_TYPE_COMPONENTS,
+		ParentNodeRefID:    0,
+		ChildrenNodeRefIDs: []int{},
+		Content:            string(cnode5str),
+	}
+
+	treeStateMap := map[int]*TreeState{1: &treeState1, 2: &treeState2, 3: &treeState3, 4: &treeState4, 5: &treeState5}
+	cnodefin := &ComponentNode{}
+	var err error
+	cnodefin, err = BuildComponentTree(&treeState1, treeStateMap, nil)
+	assert.Nil(t, err)
+	// export
+	var b []byte
+	b, err = cnodefin.Serialization()
+	assert.Nil(t, err)
+	assert.Equal(t, serilizationResult, string(b), "the serlization result should be equal")
+}
