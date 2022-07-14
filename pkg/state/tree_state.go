0d1cc89cb858b8f46646e004c989ef3b804066a0
karminski <code.karminski@outlook.com>
2022-07-14 15:56:43 +0800

    FIX: TreeStateDto.ChildrenNodeRefIDs type define typo.

--- pkg/state/tree_state.go
+++ pkg/state/tree_state.go
@@ -37,7 +37,7 @@ type TreeStateDto struct {
 	ID                 int       `json:"id"`
 	StateType          int       `json:"state_type"`
 	ParentNodeRefID    int       `json:"parent_node_ref_id"`
-	ChildrenNodeRefIDs int       `json:"children_node_ref_ids"`
+	ChildrenNodeRefIDs []int     `json:"children_node_ref_ids"`
 	AppRefID           int       `json:"app_ref_id"`
 	Version            int       `json:"version"`
 	Name               string    `json:"name"`
