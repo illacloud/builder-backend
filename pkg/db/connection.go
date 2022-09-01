23536308783af5d933ac03e83377987bae766673
karminski <code.karminski@outlook.com>
2022-09-01 11:05:00 +0800

    CHANGE: database connection info.

--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -24,8 +24,8 @@ import (
 )
 
 type Config struct {
-	Addr     string `env:"ILLA_PG_ADDR" envDefault:"dev.illasoft.com"`
-	Port     string `env:"ILLA_PG_PORT" envDefault:"31006"`
+	Addr     string `env:"ILLA_PG_ADDR" envDefault:"127.0.0.1"`
+	Port     string `env:"ILLA_PG_PORT" envDefault:"5432"`
 	User     string `env:"ILLA_PG_USER" envDefault:"illa"`
 	Password string `env:"ILLA_PG_PASSWORD" envDefault:"illa2022"`
 	Database string `env:"ILLA_PG_DATABASE" envDefault:"illa"`
