a8ff824976631d07dd3ef0891b58e14715e8835f
naj1n <dengzhanjiao@illasoft.com>
2022-12-05 19:32:53 +0800

    feat: add `CRUD` operation for `Firebase` resource & action

---
+++ pkg/plugins/firebase/service.go
@@ -0,0 +1,47 @@
+// Copyright 2022 The ILLA Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package firebase
+
+import "github.com/illa-family/builder-backend/pkg/plugins/common"
+
+type Connector struct {
+	ResourceOpts Resource
+	ActionOpts   Action
+}
+
+func (f *Connector) ValidateResourceOptions(resourceOptions map[string]interface{}) (common.ValidateResult, error) {
+	return common.ValidateResult{Valid: true}, nil
+}
+
+func (f *Connector) ValidateActionOptions(actionOptions map[string]interface{}) (common.ValidateResult, error) {
+	return common.ValidateResult{Valid: true}, nil
+}
+
+func (f *Connector) TestConnection(resourceOptions map[string]interface{}) (common.ConnectionResult, error) {
+	return common.ConnectionResult{Success: true}, nil
+}
+
+func (f *Connector) GetMetaInfo(resourceOptions map[string]interface{}) (common.MetaInfoResult, error) {
+	return common.MetaInfoResult{
+		Success: true,
+		Schema:  nil,
+	}, nil
+}
+
+func (f *Connector) Run(resourceOptions map[string]interface{}, actionOptions map[string]interface{}) (common.RuntimeResult, error) {
+	return common.RuntimeResult{
+		Success: true,
+	}, nil
+}
