05c1567aa35beb795636cf23953b327fb87066a8
karminski <code.karminski@outlook.com>
2023-01-10 21:42:31 +0800

    FINISH lookup table feature.

--- internal/websocket/cooperate.go
+++ internal/websocket/cooperate.go
@@ -8,52 +8,55 @@ const DEFAULT_ROOM_SLOT = 4
 
 type InRoomUsers struct {
 	RoomID           int
-	All              []*UserForCooperateFeedback            // []*UserForCooperateFeedback
+	All              []*UserForCooperateFeedback // []*UserForCooperateFeedback
+	AllUsers         map[int]*UserForCooperateFeedback
 	AttachedUserList map[string][]*UserForCooperateFeedback // map[component.DisplayName][]*UserForCooperateFeedback
-}
-
-type UserForCooperateFeedback struct {
-	ID       int    `json:"id"`
-	Nickname string `json:"nickname"`
-	Avatar   string `json:"avatar"`
-}
-
-func NewUserForCooperateFeedbackByUser(user *repository.User) *UserForCooperateFeedback {
-	return &UserForCooperateFeedback{
-		ID:       user.ID,
-		Nickname: user.Nickname,
-		Avatar:   repository.USER_DEFAULT_AVATAR,
-	}
 }
 
 func NewInRoomUsers(roomID int) *InRoomUsers {
 	iru := &InRoomUsers{}
 	iru.All = make([]*UserForCooperateFeedback, DEFAULT_ROOM_SLOT)
+	iru.AllUsers = make(map[int]*UserForCooperateFeedback)
 	return iru
 }
 
 func (iru *InRoomUsers) EnterRoom(user *repository.User) {
 	fuser := NewUserForCooperateFeedbackByUser(user)
 	iru.All = append(iru.All, fuser)
+	iru.AllUsers[fuser.ID] = fuser
 }
 
 func (iru *InRoomUsers) LeaveRoom(userID int) {
+	targetFuser, hit := iru.AllUsers[userID]
+	if !hit { // invalied user input, just ignore
+		return
+	}
+	// remove user in room
 	for i, fuser := range iru.All {
 		if fuser.ID == userID {
 			iru.All = append(iru.All[:i], iru.All[i+1:]...)
 			break
 		}
 	}
+	// remove user in components
+	iru.DisattachComponent(userID, targetFuser.ExportAttachedComponentDisplayName())
+	// remove from AllUsers
+	delete(iru.AllUsers, userID)
 }
 
 func (iru *InRoomUsers) AttachComponent(userID int, componentDisplayNames []string) {
-	fuser := NewUserForCooperateFeedbackByUser(user)
+	fuser, hit := iru.AllUsers[userID]
+	if !hit { // invalied user input, just ignore
+		return
+	}
 	for _, displayName := range componentDisplayNames {
 		// check if components not recorded, insert it
 		if _, hit := iru.AttachedUserList[displayName]; !hit {
 			iru.AttachedUserList[displayName] = make([]*UserForCooperateFeedback, DEFAULT_ROOM_SLOT)
 		}
 		iru.AttachedUserList[displayName] = append(iru.AttachedUserList[displayName], fuser)
+		// record user attached components
+		fuser.AttachedComponents[displayName] = displayName
 	}
 }
 
@@ -64,7 +67,7 @@ func (iru *InRoomUsers) DisattachComponent(userID int, componentDisplayNames []s
 		}
 		// remove from AttachedUserList
 		for i, fuser := range iru.AttachedUserList[displayName] {
-			if fuser.ID == user.ID {
+			if fuser.ID == userID {
 				iru.AttachedUserList[displayName] = append(iru.AttachedUserList[displayName][:i], iru.AttachedUserList[displayName][i+1:]...)
 				break
 			}
@@ -78,4 +81,29 @@ func (iru *InRoomUsers) FetchAllInRoomUsers() []*UserForCooperateFeedback {
 
 func (iru *InRoomUsers) FetchAllAttachedUsers() map[string][]*UserForCooperateFeedback {
 	return iru.AttachedUserList
+}
+
+type UserForCooperateFeedback struct {
+	ID                 int               `json:"id"`
+	Nickname           string            `json:"nickname"`
+	Avatar             string            `json:"avatar"`
+	AttachedComponents map[string]string `json:"-"`
+}
+
+func NewUserForCooperateFeedbackByUser(user *repository.User) *UserForCooperateFeedback {
+	return &UserForCooperateFeedback{
+		ID:                 user.ID,
+		Nickname:           user.Nickname,
+		Avatar:             repository.USER_DEFAULT_AVATAR,
+		AttachedComponents: make(map[string]string),
+	}
+}
+
+func (fuser *UserForCooperateFeedback) ExportAttachedComponentDisplayName() []string {
+	l := len(fuser.AttachedComponents)
+	ret := make([]string, l)
+	for _, displayName := range fuser.AttachedComponents {
+		ret = append(ret, displayName)
+	}
+	return ret
 }
