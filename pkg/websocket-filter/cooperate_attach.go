f8181cd49715210df6f0595274b76adb680cc22a
karminski <code.karminski@outlook.com>
2023-01-12 23:47:52 +0800

    FIX broacdast mode.

--- pkg/websocket-filter/cooperate_attach.go
+++ pkg/websocket-filter/cooperate_attach.go
@@ -16,7 +16,7 @@ package filter
 
 import (
 	"errors"
-
+	"fmt"
 	ws "github.com/illacloud/builder-backend/internal/websocket"
 )
 
@@ -26,6 +26,7 @@ func SignalCooperateAttach(hub *ws.Hub, message *ws.Message) error {
 	// attach components
 	inRoomUsers := hub.GetInRoomUsersByRoomID(currentClient.APPID)
 	displayNames := make([]string, 0)
+	fmt.Printf("message.Payload: %v\n", message.Payload)
 	for _, displayNameInterface := range message.Payload {
 		displayName, assertCorrectly := displayNameInterface.(string)
 		if !assertCorrectly {
@@ -39,7 +40,7 @@ func SignalCooperateAttach(hub *ws.Hub, message *ws.Message) error {
 	message.SetBroadcastType(ws.BROADCAST_TYPE_ATTACH_COMPONENT)
 	message.RewriteBroadcast()
 	message.SetBroadcastPayload(inRoomUsers.FetchAllAttachedUsers())
-	hub.BroadcastToOtherClients(message, currentClient)
+	hub.BroadcastToRoomAllClients(message, currentClient)
 
 	return nil
 }
