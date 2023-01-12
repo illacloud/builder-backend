4f70a457a239be85a747b4260e93a0d510d92dff
karminski <code.karminski@outlook.com>
2023-01-13 00:36:17 +0800

    ADD request token validator.

---
+++ internal/util/tokenvalidator/token_validator.go
@@ -0,0 +1,51 @@
+package tokenvalidator
+
+import (
+	"encoding/base64"
+	"sort"
+	
+	"crypto/md5"
+	"github.com/caarlos0/env"
+
+)
+
+type Config struct {
+	Secret string `env:"ILLA_SECRET_KEY"`
+}
+
+func GetConfig() (*Config, error) {
+	cfg := &Config{}
+	err := env.Parse(cfg)
+	return cfg, err
+}
+
+type RequestTokenValidator struct {
+	Config: *Config
+}
+
+func NewRequestTokenValidator() (*RequestTokenValidator, error) {
+	cfg, err := GetConfig()
+	if err != nil {
+		return nil errors.New("can not get config.")
+	}
+	return &RequestTokenValidator{
+		Config: cfg
+	}, nil
+}
+
+func (r *RequestTokenValidator)GenerateValidateToken(input ...string) string {
+	return GenerateValidateTokenBySliceParam(input)
+}
+
+func (r *RequestTokenValidator)GenerateValidateTokenBySliceParam(input []string) string {
+	var concatr string
+	sort.Strings(input)
+	for _, str := range input {
+		concatr += str
+	}
+	concatr += r.Config.Secret
+	hash := md5.Sum([]byte(concatr))
+	var hashConverted []byte = hash[:]
+
+	return base64.StdEncoding.EncodeToString(hashConverted)
+}
