046e7ddb00bf14ae4d8b0d99c461dc19ec1cff38
karminski <code.karminski@outlook.com>
2023-08-31 01:48:24 +0800

    ADD debug info.

--- src/response/release_app_response.go
+++ src/response/release_app_response.go
@@ -1,10 +1,13 @@
 package response
 
+import "fmt"
+
 type ReleaseAppResponse struct {
 	Version int `json:"version"`
 }
 
 func NewReleaseAppResponse(version int) *ReleaseAppResponse {
+	fmt.Printf("[DUMP] NewReleaseAppResponse: %+v\n", version)
 	resp := &ReleaseAppResponse{
 		Version: version,
 	}
@@ -12,5 +15,7 @@ func NewReleaseAppResponse(version int) *ReleaseAppResponse {
 }
 
 func (resp *ReleaseAppResponse) ExportForFeedback() interface{} {
+	fmt.Printf("[DUMP] ExportForFeedback: %+v\n", resp)
+
 	return resp
 }
