a1795621f2d572563b70d9eded5c444ea1b9f31f
karminski <code.karminski@outlook.com>
2023-10-23 19:57:57 +0800

    ADD oracle 9i resource.

---
+++ src/actionruntime/oracle9i/service.go
@@ -0,0 +1,197 @@
+// Copyright 2023 Illa Soft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package oracle
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/illacloud/builder-backend/src/actionruntime/common"
+	parser_sql "github.com/illacloud/builder-backend/src/utils/parser/sql"
+	"github.com/illacloud/builder-backend/src/utils/resourcelist"
+	"github.com/mitchellh/mapstructure"
+)
+
+type Connector struct {
+	resourceOptions Resource
+	actionOptions   Action
+}
+
+func (o *Connector) ValidateResourceOptions(resourceOptions map[string]interface{}) (common.ValidateResult, error) {
+	// format resource options
+	if err := mapstructure.Decode(resourceOptions, &o.resourceOptions); err != nil {
+		return common.ValidateResult{Valid: false}, err
+	}
+
+	// validate oracle options
+	validate := validator.New()
+	if err := validate.Struct(o.resourceOptions); err != nil {
+		return common.ValidateResult{Valid: false}, err
+	}
+
+	return common.ValidateResult{Valid: true}, nil
+}
+
+func (o *Connector) ValidateActionTemplate(actionOptions map[string]interface{}) (common.ValidateResult, error) {
+	// format action options
+	if err := mapstructure.Decode(actionOptions, &o.actionOptions); err != nil {
+		return common.ValidateResult{Valid: false}, err
+	}
+
+	// validate oracle options
+	validate := validator.New()
+	if err := validate.Struct(o.actionOptions); err != nil {
+		return common.ValidateResult{Valid: false}, err
+	}
+
+	return common.ValidateResult{Valid: true}, nil
+}
+
+func (o *Connector) TestConnection(resourceOptions map[string]interface{}) (common.ConnectionResult, error) {
+	// get oracle connection
+	db, err := o.getConnectionWithOptions(resourceOptions)
+	if err != nil {
+		return common.ConnectionResult{Success: false}, err
+	}
+	defer db.Close()
+
+	// test oracle connection
+	if err := db.Ping(); err != nil {
+		return common.ConnectionResult{Success: false}, err
+	}
+
+	return common.ConnectionResult{Success: true}, nil
+}
+
+func (o *Connector) GetMetaInfo(resourceOptions map[string]interface{}) (common.MetaInfoResult, error) {
+	// get oracle connection
+	db, err := o.getConnectionWithOptions(resourceOptions)
+	if err != nil {
+		return common.MetaInfoResult{Success: false}, err
+	}
+	defer db.Close()
+
+	// test oracle connection
+	if err := db.Ping(); err != nil {
+		return common.MetaInfoResult{Success: false}, err
+	}
+
+	columns := mapColumns(db)
+
+	return common.MetaInfoResult{
+		Success: true,
+		Schema:  columns,
+	}, nil
+}
+
+func (o *Connector) Run(resourceOptions map[string]interface{}, actionOptions map[string]interface{}, rawActionOptions map[string]interface{}) (common.RuntimeResult, error) {
+	// get Oracle connection
+	db, err := o.getConnectionWithOptions(resourceOptions)
+	if err != nil {
+		return common.RuntimeResult{Success: false}, errors.New("failed to get oracle connection")
+	}
+	defer db.Close()
+	// format query
+	if err := mapstructure.Decode(actionOptions, &o.actionOptions); err != nil {
+		return common.RuntimeResult{Success: false}, err
+	}
+	// set context field
+	errInSetRawQuery := o.actionOptions.SetRawQueryAndContext(rawActionOptions)
+	if errInSetRawQuery != nil {
+		return common.RuntimeResult{Success: false}, errInSetRawQuery
+	}
+
+	queryResult := common.RuntimeResult{Success: false}
+	queryResult.Rows = make([]map[string]interface{}, 0, 0)
+	queryResult.Extra = make(map[string]interface{})
+	err = nil
+
+	// action mode switch
+	switch o.actionOptions.Mode {
+	case ACTION_SQL_MODE:
+		sqlEscaper := parser_sql.NewSQLEscaper(resourcelist.TYPE_ORACLE_ID)
+		escapedSQL, sqlArgs, errInEscapeSQL := sqlEscaper.EscapeSQLActionTemplate(o.actionOptions.RawQuery, o.actionOptions.Context)
+		if errInEscapeSQL != nil {
+			return queryResult, errInEscapeSQL
+		}
+		// check if o.actionOptions.Opts.Raw is select query
+		isSelectQuery := false
+
+		var query SQL
+		if err := mapstructure.Decode(o.actionOptions.Opts, &query); err != nil {
+			return queryResult, errors.New("type error of action content")
+		}
+		lexer := parser_sql.NewLexer(query.Raw)
+		isSelectQuery, err = parser_sql.IsSelectSQL(lexer)
+		if err != nil {
+			return common.RuntimeResult{Success: false}, err
+		}
+
+		// fetch data
+		if isSelectQuery && o.actionOptions.IsSafeMode() {
+			rows, err := db.Query(escapedSQL, sqlArgs...)
+			if err != nil {
+				return queryResult, err
+			}
+			mapRes, err := common.RetrieveToMap(rows)
+			if err != nil {
+				return queryResult, err
+			}
+			defer rows.Close()
+			queryResult.Success = true
+			queryResult.Rows = mapRes
+		} else if isSelectQuery && !o.actionOptions.IsSafeMode() {
+			rows, err := db.Query(query.Raw)
+			if err != nil {
+				return queryResult, err
+			}
+			mapRes, err := common.RetrieveToMap(rows)
+			if err != nil {
+				return queryResult, err
+			}
+			defer rows.Close()
+			queryResult.Success = true
+			queryResult.Rows = mapRes
+		} else if !isSelectQuery && o.actionOptions.IsSafeMode() {
+			execResult, err := db.Exec(escapedSQL, sqlArgs...)
+			if err != nil {
+				return queryResult, err
+			}
+			affectedRows, err := execResult.RowsAffected()
+			if err != nil {
+				return queryResult, err
+			}
+			queryResult.Success = true
+			queryResult.Extra["message"] = fmt.Sprintf("Affeted %d rows.", affectedRows)
+		} else if !isSelectQuery && !o.actionOptions.IsSafeMode() {
+			execResult, err := db.Exec(query.Raw)
+			if err != nil {
+				return queryResult, err
+			}
+			affectedRows, err := execResult.RowsAffected()
+			if err != nil {
+				return queryResult, err
+			}
+			queryResult.Success = true
+			queryResult.Extra["message"] = fmt.Sprintf("Affeted %d rows.", affectedRows)
+		}
+	case ACTION_GUI_MODE:
+		err = errors.New("unsupported action mode")
+	default:
+		err = errors.New("unsupported action mode")
+	}
+	return queryResult, err
+}
---
+++ src/actionruntime/oracle9i/types.go
@@ -0,0 +1,82 @@
+// Copyright 2023 Illa Soft, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package oracle
+
+import (
+	"errors"
+
+	"github.com/illacloud/builder-backend/src/actionruntime/common"
+)
+
+const (
+	FIELD_CONTEXT = "context"
+	FIELD_QUERY   = "query"
+)
+
+type Resource struct {
+	Host     string `mapstructure:"host" validate:"required"`
+	Port     string `mapstructure:"port" validate:"required"`
+	Type     string `mapstructure:"connectionType" validate:"oneof=SID Service"`
+	Name     string `mapstructure:"name"`
+	SSL      bool   `mapstructure:"ssl"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+}
+
+type Action struct {
+	Mode     string                 `mapstructure:"mode" validate:"oneof=gui sql sql-safe"`
+	Opts     map[string]interface{} `mapstructure:"opts"`
+	RawQuery string
+	Context  map[string]interface{}
+}
+
+func (q *Action) IsSafeMode() bool {
+	return q.Mode == common.MODE_SQL_SAFE
+}
+
+func (q *Action) SetRawQueryAndContext(rawTemplate map[string]interface{}) error {
+	queryRaw, hit := rawTemplate[FIELD_QUERY]
+	if !hit {
+		return errors.New("missing query field for SetRawQueryAndContext() in query")
+	}
+	queryAsserted, assertPass := queryRaw.(string)
+	if !assertPass {
+		return errors.New("query field assert failed in SetRawQueryAndContext() method")
+
+	}
+	q.RawQuery = queryAsserted
+	contextRaw, hit := rawTemplate[FIELD_CONTEXT]
+	if !hit {
+		return errors.New("missing context field SetRawQueryAndContext() in query")
+	}
+	contextAsserted, assertPass := contextRaw.(map[string]interface{})
+	if !assertPass {
+		return errors.New("context field assert failed in SetRawQueryAndContext() method")
+
+	}
+	q.Context = contextAsserted
+	return nil
+}
+
+type SQL struct {
+	Raw string `mapstructure:"raw"`
+}
+
+type GUIBulkOpts struct {
+	Table   string                   `mapstructure:"table"`
+	Type    string                   `mapstructure:"actionType"`
+	Records []map[string]interface{} `mapstructure:"records"`
+	Key     string                   `mapstructure:"primaryKey"`
+}
